cca286927246180239bc8558bb2929131d553215
rsgfernandes <rsgfernandes@gmail.com>
2015-05-06 10:16:32 -0300

    first upload

---
+++ README.md
@@ -0,0 +1,12 @@
+description:
+============
+
+script to add active directory proxyaddress attribute using the same e-mail address user with different domain name.
+
+
+usage:
+======
+
+1. modify the variable $userou to use your Organizational Unit
+2. permit remote execution policy
+3. run as administrator
---
+++ add-proxyaddres.ps1
@@ -0,0 +1,14 @@
+Import-Module activedirectory
+$userou = 'OU=Organizational_Unit,DC=domain,DC=com,DC=br'
+$newproxydomain = "@youroldoralternatedomain.com.br"
+$users = Get-ADUser -Filter * -SearchBase $userou -Properties SamAccountName, EmailAddress, ProxyAddresses
+Foreach ($user in $users) {
+	If (!$user.EmailAddress) {
+		echo "$user nao possui endereco de email cadastrado no AD" > adiciona_proxyaddress.log
+	}
+	Else {
+		Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -Add @ {
+			Proxyaddresses="SMTP:"+$user.EmailAddress.split('@')[0]+""+$newproxydomain
+		}
+	}
+}
