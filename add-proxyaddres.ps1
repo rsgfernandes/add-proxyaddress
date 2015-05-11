92150772206731bb45f0322b1b57f0bd980a5561
rsgfernandes <rsgfernandes@gmail.com>
2015-05-11 07:49:54 -0300

    .

--- add-proxyaddres.ps1
+++ add-proxyaddres.ps1
@@ -2,21 +2,18 @@
 # AUTHOR  : Rodrigo Fernandes - https://br.linkedin.com/in/rsgfernandes
 # DATE    : 06-05-2015
 ########################################################### 
-
-Import-Module activedirectory
+	
+Import-Module ActiveDirectory
 $userou = 'OU=Organizational_Unit,DC=domain,DC=com,DC=br'
 $newproxydomain = "@youroldoralternatedomain.com.br"
 $defaultemailaddress = "@yourdefaultdomain.com.br"
 $users = Get-ADUser -Filter * -SearchBase $userou -Properties SamAccountName, EmailAddress, ProxyAddresses
 Foreach ($user in $users) {
-	If (!$user.EmailAddress) {
-		echo "$user nao possui endereco de email cadastrado no AD" > adiciona_proxyaddress.log
-	}
-	Else {
-		Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -Add @ {
-			Proxyaddresses="SMTP:"+$user.EmailAddress.split('@')[0]+""+$defaultemailaddress}
-		Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -Add @ {
-			Proxyaddresses="smtp:"+$user.EmailAddress.split('@')[0]+""+$newproxydomain
-		}
-	}
+                If (!$user.EmailAddress) {
+                               echo "$user nao possui endereco de email cadastrado no AD" > adiciona_proxyaddress.log
+                }
+                Else {
+				Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -Add @{Proxyaddresses="SMTP:"+$user.EmailAddress.split('@')[0]+""+$defaultemailaddress}
+				Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -Properties * | Set-ADUser -Add @{Proxyaddresses="smtp:"+$user.EmailAddress.split('@')[0]+""+$newproxydomain}
+				}
 }
